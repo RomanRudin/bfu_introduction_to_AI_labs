54f105380f2e08c4d83515f60eb2d63e4d80fdd8
Roman <rudin.roman@bk.ru>
2025-02-08 03:45:31 +0200

    Lab1 refactored, version with logarythmic scale and autorefresh was made

--- Lab1/lab_!_with_log_and_autorefresh.py
+++ Lab1/lab_!_with_log_and_autorefresh.py
@@ -6,7 +6,7 @@ from math import log
 
 #? Subplots
 fig, ((plot3, plot5), (plot6, plot6_log)) = plt.subplots(2, 2) 
-plot6_log.axis('off')
+# info.axis('off')
 
 # plot3 = plt.subplot(2, 2, 1) 
 # plot5 = plt.subplot(2, 2, 2)
@@ -16,6 +16,7 @@ plot6_log.axis('off')
 plot3.set_title('Initial points') 
 plot5.set_title('Linear regression') 
 plot6.set_title('MSE with squares') 
+plot6_log.set_title('MSE with squares and logarythmic scale') 
 
 
 def plot_initial_data(x: list, y: list, chosen_columns_variant: tuple[str, str]) -> None:
@@ -71,6 +72,50 @@ def plot_regression_with_squares(x: list, y: list, w0: int, w1: int, chosen_colu
         plot6.add_patch(patch)
 
 
+def plot_regression_with_squares_logarythmic(x: list, y: list, w0: int, w1: int, chosen_columns_variant: tuple[str, str]) -> None:
+    def __plot_regression_with_squares_logarythmic(x, y, w0, w1) -> None:
+        #? Unable to use .axline() for logarythmic scale, so trying another thing here:
+        x_new = np.linspace(min(0, min(x) * 1.2), max(0, max(x) * 1.2), 1000)
+        plot6_log.plot(x_new, w0 + w1 * x_new, color='red')
+        #? Draw and fill error squares
+        for i in range(len(x)):
+            if (y[i] < y_pred[i]):
+                patch = patches.Rectangle((x[i], y[i]), -abs(y_pred[i] - y[i]) * difference_between_scales, abs(y_pred[i] - y[i]), color='green', alpha=0.4)
+            else:
+                patch = patches.Rectangle((x[i], y_pred[i]), abs(y_pred[i] - y[i]) * difference_between_scales, abs(y_pred[i] - y[i]), color='green', alpha=0.4)
+            plot6_log.add_patch(patch)
+
+    plot6_log.scatter(x, y, color='blue')
+    plot6_log.set_xlabel(chosen_columns_variant[0])
+    plot6_log.set_ylabel(chosen_columns_variant[1])
+    y_pred = [w0 + w1 * xi for xi in x]
+
+    for patch in plot6_log.patches:
+        patch.remove()
+
+    #? Size of the figure
+    bbox = plot6.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    #? Difference between scales inculding difference between sizes of axes
+    difference_between_scales = (plot6.get_xlim()[1] - plot6.get_xlim()[0]) / (plot6.get_ylim()[1] - plot6.get_ylim()[0]) / (bbox.width / bbox.height)
+
+    if difference_between_scales < 1:
+        plot6_log.set_yscale('log')
+    else:
+        plot6_log.set_xscale('log')
+
+    __plot_regression_with_squares_logarythmic(x, y, w0, w1)
+
+
+def on_resize(x: list, y: list, w0: int, w1: int) -> None:
+    plt.ion()
+    plot_initial_data(x, y, columns_variants[chosen_variant - 1])
+    plot_regression(x, y, w0, w1, columns_variants[chosen_variant - 1])
+    plot_regression_with_squares(x, y, w0, w1, columns_variants[chosen_variant - 1])
+    plot_regression_with_squares_logarythmic(x, y, w0, w1, columns_variants[chosen_variant - 1])
+    fig.canvas.draw()
+    plt.ioff()
+
+
 
 if __name__ == "__main__":
     #? Ex1
@@ -108,6 +153,10 @@ if __name__ == "__main__":
     plot_regression(x, y, w0, w1, columns_variants[chosen_variant - 1])
     #? Ex6
     plot_regression_with_squares(x, y, w0, w1, columns_variants[chosen_variant - 1])
+    #plot6_log.axis("off")
+    plot_regression_with_squares_logarythmic(x, y, w0, w1, columns_variants[chosen_variant - 1])
+
+    fig.canvas.mpl_connect('resize_event', lambda func: on_resize(x, y, w0, w1))
 
 
     #? General
